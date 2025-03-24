5e76128324cd75c71ba57e5659b82a71169585db
Adones <adonesshpep@gmail.com>
2025-03-24 20:17:12 +0300

    some changes to the dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <title>Document</title>
     <script src='https://cdn.tailwindcss.com'></script>
 </head>
+
 <body>
     <div class="min-h-full">
         <nav class="bg-gray-800">
@@ -16,37 +18,44 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Dashboard</a>
-                                <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Profile</a>
-                                <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Settings</a>
-                                <button form="logout" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Log Out</button>
+                                <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                                    aria-current="page">Dashboard</a>
+                                <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                                    aria-current="page">Profile</a>
+                                <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                                    aria-current="page">Settings</a>
+                                <button form="logout"
+                                    class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                                    aria-current="page">Log Out</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </nav>
-    
+
         <header class="bg-white shadow-sm">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
             </div>
         </header>
         <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <x-slot:head>
-                        Users
-                    </x-slot:head>
-                    <div class="space-y-4">
-                        @foreach ($users as $user)
-                            <div>
-                                name={{$user->name}} : email={{$user->email}}
-                            </div>
-                        @endforeach
+            <x-slot:head>
+                Uers
+            </x-slot:head>
+            <div class="space-y-4">
+                @foreach ($users as $user)
+                <a href="/dashboard/{{$user->id}}" class="block px-4 py-6 border border-gray-600 rounded-lg">
+                    <div class="font-bold text-blue-500 text-sm">{{$user->name}}</div>
+                    <div>
+                        email : {{$user->email}}
                     </div>
+                </a>
+                @endforeach
             </div>
         </main>
     </div>
-    <form method="POST" action="dashboard/logout" class="hidden" id="logout">@csrf</form>
+    <form method="POST" action="/logout" class="hidden" id="logout">@csrf</form>
 </body>
+
 </html>
